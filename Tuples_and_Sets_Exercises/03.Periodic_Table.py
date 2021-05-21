0dcbf0d6452b86fa1a5febb4e83ec6627ccf1468
svetoslava <svetoslava_stoyanova92@abv.bg>
2021-05-21 14:09:01 +0300

    new file

---
+++ Tuples_and_Sets_Exercises/03.Periodic_Table.py
@@ -0,0 +1,6 @@
+numbers = int(input())
+all_elements = set()
+for _ in range(numbers):
+    elements = set(input().split())
+    all_elements = all_elements.union(elements)
+print('\n'.join(all_elements))
