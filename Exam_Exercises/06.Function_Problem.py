6ce744c196fd4c33e30ed147da667232d353f492
svetoslava <svetoslava_stoyanova92@abv.bg>
2021-06-13 09:37:48 +0300

    new file

---
+++ Exam_Exercises/06.Function_Problem.py
@@ -0,0 +1,14 @@
+def get_magic_triangle(n):
+    matrix = []
+    for row in range(1, n+1):
+        for col in range(row):
+            col = 0
+            matrix.append(col)
+
+
+
+
+
+    return matrix
+
+print(get_magic_triangle(5))
