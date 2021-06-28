88014cd95c59045c4573a715e0680eb70dc1b771
svetoslava <svetoslava_stoyanova92@abv.bg>
2021-06-28 16:07:45 +0300

    new files added

---
+++ Functions_Advanced_Exercises/01.Smallest of three.py
@@ -0,0 +1,9 @@
+def smallest_number(first, second, third):
+    return(min(first, second, third))
+
+
+first = int(input())
+second = int(input())
+third = int(input())
+
+print(smallest_number(first, second, third))
