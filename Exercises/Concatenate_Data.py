0e3d7e9cb9971dfc6ce46a71dd43a07fc441e60c
svetoslava <svetoslava_stoyanova92@abv.bg>
2021-07-18 17:43:16 +0300

    new file

---
+++ Exercises/Concatenate_Data.py
@@ -0,0 +1,8 @@
+architect_name = input()
+project_counts = int(input())
+one_project = 3
+
+hours_count = one_project*project_counts
+
+print(f'The architect {architect_name} will need {hours_count} hours to complete {project_counts} project/s. ')
+
